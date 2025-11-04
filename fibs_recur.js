4c8730cde1067e8326d9c23914179a1785db5f49
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-11-04 13:40:19 -0500

    working for n >= 2

--- fibs_recur.js
+++ fibs_recur.js
@@ -1,11 +1,22 @@
-let fib_array = []
-
 function fibs(n) {
-    if (n==0) {
-        return fib_array
+    if (n <= 2) {
+        return [0, 1]
     } else {
-        fib_array[n] = fibs(n-1) + fibs(n-2)
+        let previous = fibs(n-1)
+        let lengthOfPrevious = previous.length
+        let nextNumber = previous[lengthOfPrevious-1] + previous[lengthOfPrevious-2]
+        previous.push(nextNumber)
+        return previous
+
     }
 }
 
+console.log(fibs(0))
+console.log(fibs(1))
+console.log(fibs(2))
 console.log(fibs(3))
+console.log(fibs(4))
+console.log(fibs(5))
+console.log(fibs(6))
+console.log(fibs(7))
+console.log(fibs(8))
